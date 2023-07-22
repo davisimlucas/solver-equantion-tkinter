b1fb975f8e02d535a2831daee36fc5ef883049d1
davisimlucas <davisimoeslucas@gmail.com>
2023-07-21 23:16:56 -0300

    solver equation logic

--- solverLogic/solverEquation.py
+++ solverLogic/solverEquation.py
@@ -1,57 +1,56 @@
-class solverEq:
 
-    oppositeOperators = {
-        '+': '-',
-        '-': '+',
-        '*': '/',
-        '/': '*'
-    }
+oppositeOperators = {
+    '+': '-',
+    '-': '+',
+    '*': '/',
+    '/': '*'
+}
 
-    def checkIncognito(valor):
-        try:
-            float(valor)
-            return False
-        except:
-            return True
+def checkIncognito(valor):
+    try:
+        float(valor)
+        return False
+    except:
+        return True
 
-    def solverEquation(equation):
+def solverEquation(equation):
 
-        parts = equation.split('=')
-        expression = equation[1]
-        validOperators = ['+', '-', '*', '/']
+    parts = equation.split('=')
+    expression = equation[1]
+    validOperators = ['+', '-', '*', '/']
 
-        for op in validOperators:
-            value = expression.split(op)
-            if len(value) > 1:
-                operators = op
-                break
+    for op in validOperators:
+        value = expression.split(op)
+        if len(value) > 1:
+            operators = op
+            break
         
-        values = [v.strip() for v in values]
-        leftValue = parts[0].strip()
+    values = [v.strip() for v in values]
+    leftValue = parts[0].strip()
 
-        if checkIncognito(leftValue):
+    if checkIncognito(leftValue):
+        A = float(values[0])
+        B = float(values[1])
+    elif checkIncognito(values[0]):
+        A = float(leftValue)
+        B = float(values[1])
+    elif checkIncognito(value[1]):
+        if op == '-' or op == '/':
             A = float(values[0])
-            B = float(values[1])
-        elif checkIncognito(values[0]):
+            B == float(leftValue)
+        elif op == '+' or op == '*':
             A = float(leftValue)
-            B = float(values[1])
-        elif checkIncognito(value[1]):
-            if op == '-' or op == '/':
-                A = float(values[0])
-                B == float(leftValue)
-            elif op == '+' or op == '*':
-                A = float(leftValue)
-                B = float(values[0])
-
-        if op == '+':
-            result = A - B 
-            return result
-        elif op == '-':
-            result = A + B
-            return result
-        elif op == '*':
-            result = A / B 
-            return result
-        else: 
-            result = A * B
-            return result
+            B = float(values[0])
+            
+    if op == '+':
+        result = A - B 
+        return result
+    elif op == '-':
+        result = A + B
+        return result
+    elif op == '*':
+        result = A / B 
+        return result
+    else: 
+        result = A * B
+        return result
